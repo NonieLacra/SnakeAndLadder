99d4c4bfb023e13858bb1ebeacd03baa95ce3dbd
NonieLacra <nonielacra@gmail.com>
2023-03-29 10:15:56 +0800

    Initial Commit of Snake and Ladder

--- app/src/main/java/ph/stacktrek/novare/ecommercenovare/lacra/snakeandladder/MainFragment.kt
+++ app/src/main/java/ph/stacktrek/novare/ecommercenovare/lacra/snakeandladder/MainFragment.kt
@@ -5,36 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
-import ph.stacktrek.novare.ecommercenovare.lacra.snakeandladder.databinding.FragmentMainBinding
+import android.widget.Button
 
 
 class MainFragment : Fragment() {
 
-    lateinit var binding: FragmentMainBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_main, container, false)
+        val toFrag1Btn: Button = view.findViewById(R.id.Btn1)
+        val toFrag2Btn: Button = view.findViewById(R.id.Btn2)
+        val toFrag3Btn: Button = view.findViewById(R.id.Btn3)
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding = FragmentMainBinding.inflate(layoutInflater)
-        val view = binding.root
-
-
-
-        binding.fragment1Btn.setOnClickListener{
-            Navigation.findNavController(view).navigate(R.id.action_mainFragment_to_fragment1)
+        toFrag1Btn.setOnClickListener(){
+            val fragment = Fragment1()
+            val transaction = fragmentManager?.beginTransaction()
+            transaction?.replace(R.id.fragmentContainerView,fragment)?.commit()
         }
 
-        binding.fragment2Btn.setOnClickListener{
-            Navigation.findNavController(view).navigate(R.id.action_mainFragment_to_fragment2)
+        toFrag2Btn.setOnClickListener(){
+            val fragment = Fragment2()
+            val transaction = fragmentManager?.beginTransaction()
+            transaction?.replace(R.id.fragmentContainerView,fragment)?.commit()
         }
 
-
+        return view
     }
+
+
 }
